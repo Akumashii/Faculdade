f956fd2ad8a2a0b5ed8e5ae8457c6ba77f1c5adb
Frazzon <161648912+Akumashii@users.noreply.github.com>
2025-10-17 11:48:34 -0300

    Update aula 9 CODIGO 251017.sql

--- Faculdade/4 semestre/ Implementação de Banco de Dados/aula 9  CODIGO 251017.sql
+++ Faculdade/4 semestre/ Implementação de Banco de Dados/aula 9  CODIGO 251017.sql
@@ -94,7 +94,185 @@ UPDATE FUNCIONARIO
 SET Pnome = 'Aura'
 WHERE Cpf = '13242353419';
 
-/*
-	Crie um trigger que não deixe inserir um funcionário
-	que tenha o nome completo que um já existente
+/* ROLLBACK TRIGGER
+	Trigger para evitar inserção de nomes duplicados
 */
+GO
+CREATE OR ALTER TRIGGER trg_AfterNomesDuplicados
+ON FUNCIONARIO
+AFTER INSERT
+AS
+BEGIN
+	DECLARE @Duplicados INT;
+	-- antes de dar COMMIT (acao irreversivel) eu vejo se ta tudo certo com o banco
+	-- se estiver conforme COMMIT, se não ROLLBACK
+
+	SELECT @Duplicados = COUNT(*)
+	FROM(
+			SELECT Pnome, Unome, Minicial
+			FROM FUNCIONARIO 
+			GROUP BY Pnome, Unome, Minicial
+			HAVING COUNT(*) > 1
+		) AS Duplicados;
+
+	IF @Duplicados > 0
+	BEGIN
+		PRINT 'Há duplicata no banco';
+		ROLLBACK TRANSACTION;
+	END 
+	ELSE
+	BEGIN
+		PRINT 'Sem duplicatas no banco';
+		COMMIT TRANSACTION;
+	END;
+END;
+GO
+
+SELECT Cpf, Pnome, Unome, Minicial
+FROM FUNCIONARIO;
+
+INSERT INTO FUNCIONARIO(Cpf, Pnome, Unome, Minicial) 
+VALUES ('112363411',	'Carlos',	'Ferreira',	'M');
+
+/* Segundo jeito de fazer o de cima
+
+*/
+
+GO
+CREATE OR ALTER TRIGGER trg_AfterNomesDuplicados
+ON FUNCIONARIO
+INSTEAD OF INSERT
+AS
+BEGIN
+	DECLARE @Pnome VARCHAR(50),
+			@Minicial CHAR(1),
+			@Unome VARCHAR(50),
+			@Duplicados INT;
+	SELECT @Pnome = Pnome, @Unome = Unome, @Minicial = Minicial
+	FROM INSERTED;
+
+	-- Verifica se existe algum com o mesmo nome
+	IF EXISTS( 
+			SELECT 1 
+			FROM FUNCIONARIO
+			WHERE(
+				@Pnome = Pnome
+				AND @Unome = Unome
+				AND @Minicial = Minicial				
+				)
+			)
+	BEGIN
+		PRINT 'Já existe alguém com este nome...'
+		-- RAISERROR( 'Mensagem de ERRO', Severidade do ERRO, Estado)  
+		 /* Nível de Severidade: 
+
+			0 a 10: Informativo ou advertências que não causam interrupção na execução. 
+			Geralmente usados para mensagens de log ou alertas leves.
+
+			11 a 16: Erros que indicam um problema com a consulta, como violação de regras de negócios ou erros de sintaxe. 
+			Esses erros podem interromper a execução da consulta atual, mas não comprometem o SQL Server em si.
+		
+			17 a 25: Erros graves ou críticos que indicam falhas no servidor, recursos ou corrupção de dados. 
+			Esses erros podem interromper transações ou até mesmo causar falhas no servidor.
+		 
+		 */
+		RAISERROR('Está com NOMES duplicados', 16, 0);
+	END
+	ELSE
+	BEGIN
+		INSERT INTO FUNCIONARIO(Pnome, Minicial, Unome, Cpf, Datanasc, Endereco, Sexo, Salario, Cpf_supervisor, Dnr, Bonus, Data_Admissao)
+		SELECT Pnome, Minicial, Unome, Cpf, Datanasc, Endereco, Sexo, Salario, Cpf_supervisor, Dnr, Bonus, Data_Admissao
+		FROM INSERTED;
+	END
+END;
+GO
+
+SELECT * FROM FUNCIONARIO ORDER BY Pnome;
+
+INSERT INTO FUNCIONARIO(Cpf, Pnome, Unome, Minicial)
+VALUES ('99999999', 'Frazzon', 'Frazzon', 'F');
+
+/*
+	CREATE TABLE Log_Funcionario (
+		LogID INT IDENTITY(1,1) PRIMARY KEY,
+		Cpf CHAR(11),
+		Operacao VARCHAR(10),
+		Data_Hora DATETIME DEFAULT GETDATE()
+	);
+
+Crie um trigger que seja disparado depois que uma
+operação de inserção ocorra na tabela
+FUNCIONARIO. Esse trigger deve registrar o CPF do
+novo funcionário inserido e a operação realizada
+(neste caso, "INSERT") em uma tabela de log
+(Log_Funcionario), juntamente com a data e hora da
+inserção. Esse trigger ajudará a manter um histórico
+das inserções realizadas na tabela de funcionários.
+*/
+
+CREATE TABLE Log_Funcionario (
+		LogID INT IDENTITY(1,1) PRIMARY KEY,
+		Cpf CHAR(11),
+		Operacao VARCHAR(10),
+		Data_Hora DATETIME DEFAULT GETDATE()
+);
+
+GO
+CREATE OR ALTER TRIGGER trg_InserçãoFuncionario
+ON FUNCIONARIO
+AFTER INSERT
+AS 
+BEGIN
+	INSERT INTO Log_Funcionario(Cpf, Operacao)
+	SELECT Cpf, 'INSERT'
+	FROM INSERTED;
+END
+GO
+
+-- TRIGGER UPDATE
+
+GO
+CREATE OR ALTER TRIGGER trg_UpdateFuncionario
+ON FUNCIONARIO
+AFTER UPDATE
+AS 
+BEGIN
+	
+	INSERT INTO Log_Funcionario(Cpf, Operacao)
+	SELECT Cpf, 'UPDATE'
+	FROM inserted;
+	
+END
+GO
+
+-- TRIGGER DELETE
+
+GO
+CREATE OR ALTER TRIGGER trg_DeleteFuncionario
+ON FUNCIONARIO
+AFTER DELETE
+AS 
+BEGIN
+	
+	INSERT INTO Log_Funcionario(Cpf, Operacao)
+	SELECT Cpf, 'DELETE'
+	FROM deleted;
+	
+END
+GO
+
+-- TRIGGER INSERT UPDATE DELETE
+
+GO
+CREATE OR ALTER TRIGGER trg_UpdateFuncionario
+ON FUNCIONARIO
+AFTER UPDATE, DELETE, INSERT
+AS 
+BEGIN
+	
+	INSERT INTO Log_Funcionario(Cpf, Operacao)
+	SELECT Cpf, 'UPDATE'
+	FROM INSERTED;
+	
+END
+GO
